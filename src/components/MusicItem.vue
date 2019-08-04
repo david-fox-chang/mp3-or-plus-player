097aa689e1e2090aaaffd504068736f70f2999b3
David <david.fox0904@gmail.com>
2019-08-04 17:45:17 +0800

    MusicItem component built

---
+++ src/components/MusicItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="music-item">
+    <span class="music-name">{{ music.name }}</span>
+    <span class="music-artist">{{ music.artist }}</span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    music: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
--- src/views/PlayList.vue
+++ src/views/PlayList.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="play-list">
     <h1>play-list</h1>
+    <ul class="list">
+      <music-item
+        v-for="music in playlists" :key="`${music.id}-${music.randomId}`"
+        :music="music"
+      ></music-item>
+    </ul>
   </div>
 </template>
 
 <script>
 import { mapState, mapGetters } from 'vuex';
+import MusicItem from '../components/MusicItem.vue';
 
 export default {
 
+  components: {
+    'music-item': MusicItem,
+  },
   computed: {
     ...mapState([
       'playlists',
