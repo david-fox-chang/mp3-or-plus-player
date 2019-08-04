eb600592c5c171492914562014091ecaa165cc19
David <david.fox0904@gmail.com>
2019-08-04 17:46:17 +0800

    storage names extracted

---
+++ src/mutation-types.js
@@ -0,0 +1,10 @@
+const PLAYER_TIME = 'playerTime';
+const PLAYER_TIMER = 'playerTimer';
+const PLAYER_COUNT_DOWN = 'playerCountDown';
+
+export {
+  // eslint-disable-next-line import/prefer-default-export
+  PLAYER_TIME,
+  PLAYER_TIMER,
+  PLAYER_COUNT_DOWN,
+};
